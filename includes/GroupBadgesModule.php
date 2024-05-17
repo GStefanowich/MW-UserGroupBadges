dc8d3a7271daee83ccb25c93ded178b0853034d6
alex4401 <rylatgl@gmail.com>
2024-05-17 23:57:08 +0200

    Implement CSS generation in GroupBadgesModule

--- includes/GroupBadgesModule.php
+++ includes/GroupBadgesModule.php
@@ -1,8 +1,6 @@
 <?php
 namespace MediaWiki\Extension\UserGroupBadges;
 
-use MediaWiki\Extension\ThemeToggle\ExtensionConfig;
-use MediaWiki\Extension\ThemeToggle\ThemeAndFeatureRegistry;
 use MediaWiki\MediaWikiServices;
 use MediaWiki\ResourceLoader\Context;
 use MediaWiki\ResourceLoader\FileModule;
@@ -10,6 +8,19 @@ use MediaWiki\ResourceLoader\FileModule;
 class GroupBadgesModule extends FileModule {
     public function getStyles( Context $context ) {
         $styles = parent::getStyles( $context );
+
+        /** @var UserGroupBadges */
+        $badges = MediaWikiServices::getInstance()->getService( UserGroupBadges::SERVICE_NAME );
+
+		// Loop through our groups
+        $badgeCss = [];
+		foreach ( $badges->getGroups() as $group => $data ) {
+			$badgeCss[] = 'i.group-badge.role-' . $group . '{ background-image: url("' . $data['url'] . '") }';
+		}
+
+        // Add our generated CSS to the bundle (for all media types).
+		$styles['all'] = ( $styles['all'] ?? '' ) . "\n" . implode( '', $badgeCss );
+
         return $styles;
     }
 
